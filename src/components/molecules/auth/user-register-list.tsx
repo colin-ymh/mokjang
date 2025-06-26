fa803db26745fc6673230d0c8b480612f9bde638
colin-ymh <dbals1120@gmail.com>
2025-06-26 14:43:34 +0900

    .

--- src/components/molecules/auth/user-register-list.tsx
+++ src/components/molecules/auth/user-register-list.tsx
@@ -77,7 +77,7 @@ const UserRegisterList = () => {
         {
           name,
           mobilePhone: mobilePhone.replace(/-/g, ''),
-          isTest: IS_TEST.INTERNAL_TEST,
+          isTest: IS_TEST.PRODUCTION,
         }
       );
 
