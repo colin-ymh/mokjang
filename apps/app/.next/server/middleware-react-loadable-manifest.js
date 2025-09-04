b88a2d518636437dad5be8229513a2e87dfe39f6
colin-ymh <dbals1120@gmail.com>
2025-09-04 10:13:11 +0900

    cache

--- apps/app/.next/server/middleware-react-loadable-manifest.js
+++ apps/app/.next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../locales/client.ts -> ./en\":{\"id\":\"../locales/client.ts -> ./en\",\"files\":[\"static/chunks/_app-pages-browser_locales_en_ts.js\"]},\"../locales/client.ts -> ./ko\":{\"id\":\"../locales/client.ts -> ./ko\",\"files\":[\"static/chunks/_app-pages-browser_locales_ko_ts.js\"]}}"
+self.__REACT_LOADABLE_MANIFEST='{"../locales/client.ts -> ./en":{"id":9771,"files":["static/chunks/771.6472a7279e4497c9.js"]},"../locales/client.ts -> ./ko":{"id":16623,"files":["static/chunks/623.46112d705c525edd.js"]}}';
--- apps/landing/.next/server/middleware-react-loadable-manifest.js
+++ apps/landing/.next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST='{"../locales/client.ts -> ./en":{"id":9771,"files":["static/chunks/771.6d5ffaf3be8e4572.js"]},"../locales/client.ts -> ./ko":{"id":6623,"files":["static/chunks/623.569df0925763d824.js"]}}';
