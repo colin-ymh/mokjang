714f114cca0aba9696a7dce0f514cf389dafd95c
colin-ymh <dbals1120@gmail.com>
2025-09-19 12:48:10 +0900

    next cache 제거

--- apps/landing/src/utils/router.ts
+++ apps/landing/src/utils/router.ts
@@ -1,3 +1,3 @@
 export const routeAppPage = (page: string) => {
-  window.location.href = `${process.env.NEXT_PUBLIC_APP_CLIENT_PROTOCOL}://${process.env.NEXT_PUBLIC_APP_CLIENT_HOST}:${process.env.NEXT_PUBLIC_APP_CLIENT_PORT}${page}`;
+  window.location.href = `${process.env.NEXT_PUBLIC_APP_CLIENT_PROTOCOL}://${process.env.NEXT_PUBLIC_APP_CLIENT_HOST}${page}`;
 };
